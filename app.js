4e28754d52978909fcbb3cb6e1277b89e0988b7d
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-02-05 15:07:53 -0800

    Integrated Socket.io

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express();
 //Socket.io connect to http server
 const server =  require('http').Server(app);
 
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("New user connected!");
+})
+
 //express view engine for handlbars
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+//establish public folder
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) =>{
     res.render('index.handlebars');
